672a0ccbe8b67e2c5b1489fc432d62742d6f48b3
franciowel <spina_francesco@blu.it>
2022-06-07 14:48:13 +0200

    first commit

---
+++ MILESTONE 1/js/script.js
@@ -0,0 +1,8 @@
+// MILESTONE 1
+// Per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// // Stampare in console
+
+// START
+for(let i = 0; i <= 100; i++)
+console.log(i)
